3728f34528482d3b1c111ebd792a752ceae7e0d3
professorsoares <professorsoares@gmail.com>
2021-07-22 11:30:24 -0300

    Implementação de uma segunda ideia que tive, passando uma lista de argumentos variável.

--- desafioMaionumero.py
+++ desafioMaionumero.py
@@ -7,7 +7,16 @@ def retornaMaiorNumero(num1, num2, num3):
     arrNumero = num1, num2, num3
     return(sorted(arrNumero)[len(arrNumero)-1])
 
+def retornaMaiorNumeroV2(*num):
+    return(sorted(num)[len(num)-1])
+
 
 if __name__ == "__main__":
     num = retornaMaiorNumero(22,30,10)
-    print(num)
+    print("1.Maior Número V1: {}".format(num))
+
+    num2 = retornaMaiorNumeroV2(22,30,10)   # Recebe qualquer quantidade de números(parâmetros).
+    print("2.Maior Número V2: {}".format(num2))
+
+    num2 = retornaMaiorNumeroV2(22,30,10,50,2,5)
+    print("3.Maior Número V2: {}".format(num2))
